fa334560cab8473f8a14978b1cbf8aa08c39e230
Ebenezer Menyaga <menyagaebenezer@gmail.com>
2022-11-13 16:52:11 +0100

    feat: setup routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./app.css";
 
+function HomePage() {
+  return "home page";
+}
+
+function PlacesPage() {
+  return "places page";
+}
+
 function App() {
-  return <>Hello world</>;
+  return (
+    <Routes>
+      <Route path="/metabnb" element={<HomePage />} />
+      <Route path="/metabnb/places" element={<PlacesPage />} />
+    </Routes>
+  );
 }
 
 export default App;
