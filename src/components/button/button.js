b7f8fb1a0b98be991346ca044852a31b577cdace
Ebenezer Menyaga <menyagaebenezer@gmail.com>
2022-11-13 22:16:29 +0100

    feat: add button light type

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -2,10 +2,10 @@ import { Util } from "../../shared/util";
 import "./button.css";
 
 export function Button(props) {
-  const { className, isIconButton, ...buttonProps } = props;
+  const { className, isIconButton, isLight, ...buttonProps } = props;
   const classNames = Util.classNames(
     "button",
-    { "button--icon-only": isIconButton },
+    { "button--icon-only": isIconButton, "button--light": isLight },
     className
   );
 
